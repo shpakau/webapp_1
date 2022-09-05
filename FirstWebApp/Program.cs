a692ea7efdbacff594698133de715ee5e5958066
shpakau <shpakau@yahoo.com>
2022-09-05 11:04:08 +0300

    all

--- FirstWebApp/Program.cs
+++ FirstWebApp/Program.cs
@@ -1,12 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Привет");
+// app.MapGet("/", () => "Привет");
 
-app.MapGet("/about", () => RouteMethods.About());
+// app.MapGet("/about", () => RouteMethods.About());
 
-app.MapGet("/hi/{name}", (string name) => RouteMethods.Hi(name));
+// app.MapGet("/hi/{name}", (string name) => RouteMethods.Hi(name));
 
-app.MapGet("/calc/{x},{y}", (int x, int y) => RouteMethods.Sum(x, y));
+// app.MapGet("/calc/{x},{y}", (int x, int y) => RouteMethods.Sum(x, y));
+
+////  третья часть
+
+app.UseRouting();
+
+app.UseEndpoints(endpoint => 
+  endpoint.MapControllerRoute(
+      name: "Default",
+      pattern: "api/{controller}/{action}/{arg?}"
+      )
+  );
 
 app.Run();
